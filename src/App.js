867197a14d51a0f9db6b560374f0d5f4cfd8107c
Nati.T <tibebunate@gmail.com>
2021-11-12 13:41:53 +0300

    broken down home pages into direcotry and menu-item compoents and also added some navigation ablitly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage.componet';
+import HomePage from './pages/homepage/homepage.componet';
+import {Switch,Route} from 'react-router-dom';
+
+
+const Hats = () => (
+  <h1>Hats</h1>
+
+);
 
 function App() {
   return( 
    <div> 
-     <HomePage />
+     <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop/hats" component={Hats} />
+     </Switch>
    </div>
   )
 }
---
+++ src/pages/homepage/homepage.componet.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './homepage.styles.scss';
+import Direcotry from '../directory-menu/directory-menu.componet'
+
+const HomePage = () => (
+    <div className="homepage">
+        <Direcotry />
+    </div>
+)
+
+export default HomePage;
+
+
+
+
+
+
+
